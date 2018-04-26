4e01f4e353ac275d6b000186bd7bcfd1d92c680b
ethanryan <ethanryan@gmail.com>
2018-04-25 20:34:19 -0400

    added plot_url_params to plot_urls_controller

--- app/controllers/plot_urls_controller.rb
+++ app/controllers/plot_urls_controller.rb
@@ -5,8 +5,12 @@ class PlotUrlsController < ApplicationController
   end
 
   def create
-    plot_url = PlotUrl.new(url_params)
+    plot_url = PlotUrl.new(plot_url_params)
     plot_url.save
+  end
+
+  def plot_url_params
+    params.require(:plot_url).permit(:title, :link, :summary, :genre, :cast, :plot)
   end
 
 end
