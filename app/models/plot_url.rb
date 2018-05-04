026a55f7b64708caff810862b368fcaba332c056
ethanryan <ethanryan@gmail.com>
2018-05-03 21:43:10 -0400

    method parseHTML works! takes url to wikipedia movie as input, and outputs a mostly done seed file in JSON for wordnerds backend

--- app/models/plot_url.rb
+++ app/models/plot_url.rb
@@ -1,6 +1,6 @@
 class PlotUrl < ApplicationRecord
 
-
+  #below lines for copying and pasting and testing in console, by calling method on a link...
   p = PlotUrl.new
   link_1 = "https://en.wikipedia.org/wiki/The_Shining_(film)"
 
@@ -54,10 +54,7 @@ class PlotUrl < ApplicationRecord
     plot_array.shift #remove "Plot[edit]" from plot_array
     plot_array.pop #remove "Cast[edit]" from plot_array
 
-    # plot_array = plot_array.map.with_index do |element, index|
-    #   "index: " + index.to_s
-    #   "text: " + element.text
-    # end #keep working on this, maybe make another method that gets called...
+    #maybe make another method that gets called for below...
 
     plot_array = plot_array.map.with_index do |element, index|
       order = index + 1
@@ -72,16 +69,14 @@ class PlotUrl < ApplicationRecord
     end
 
     plot_array = plot_array.map{|h| Array.new << h} #put each hash in an array, making plot_array an array of arrays, each with one hash...
+    self.plot = plot_array
 
-    array1 = Array.new
-    array1 << plot_array #nesting plot_array in an array........
+    file_content_array = Array.new
+    file_content_array << plot_array #nesting plot_array in an array........
     #puts "plot_array contains: " #wanna check it out in the console, but don't need this...
     #plot_array.map { |element| puts element }
 
-    #self.save  #this will save each created plot to the database...
-    #file_name = title
-    #file_content = plot_array
-    create_file(title, array1) #calling method below with two arguments
+    create_file(title, file_content_array) #calling method below with two arguments
 
     byebug #<<<<-------- byebug works! keep on byebubbing...
   end
@@ -103,12 +98,17 @@ class PlotUrl < ApplicationRecord
     require 'fileutils'
     file_name = file_name + ".rb"
     somefile = File.open(file_name, "w") #File.open is same as File.new, it creates a new file...
-    comment = "#select array within Paragraph.create(), then click: Packages > Pretty JSON > Prettify\n"
-    file_content = file_content.join(",")
-    file_content = "paragraphs = Paragraph.create([" + file_content + "])"
-    file_content = comment + file_content
+    comment_1 = "#select array within Plot.create(), then click: Packages > Pretty JSON > Prettify\n"
+    plot_content = "plot = Plot.create([{\"genre_id\": \"null\", \"title\": \"#{file_name}\", \"author\": \"null\"}])\n\n" #note: need to escape all double quotes within double quotes, for interpolated file name, and need double quotes for all key values anyway
+
+    comment_2 = "#select array within Paragraph.create(), then click: Packages > Pretty JSON > Prettify\n"
+
+    paragraph_content = file_content.join(",")
+    paragraph_content = "paragraphs = Paragraph.create([" + paragraph_content + "])"
+
+    final_file_content = comment_1 + plot_content + comment_2 + paragraph_content
     #adding comma to end of each hash, now just need to put it all in brackets! <<<<-----!!!!!
-    somefile.puts file_content
+    somefile.puts final_file_content
     somefile.close
     puts "created file..."
     return "created file...!!!!!"
@@ -118,7 +118,7 @@ class PlotUrl < ApplicationRecord
   #function to call from controller, to call function above...
   def parse_that_ish(link)
     self.parseHTML(link)
-    self.save
+    self.save #this will save each created plot to the database...
   end
 
 end #end of class
